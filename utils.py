e626e98c13eab5e0b6955d573098b8f18234fc94
Ahmadreza Hadi <ahmadreza.hadi7@gmail.com>
2022-10-19 15:25:02 +0330

    added lr schedule function

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from cmath import inf
-from environment import Job
-from environment import Env
+from typing import Callable
+from envs.deepcss_v0.environment import Job, Env
 
 
 def calculate_average_slowdown(info):
@@ -25,3 +25,26 @@ def SJF(env: Env):
     if action is None:
         return 0
     return action
+
+
+def linear_schedule(initial_value: float, final_value: float) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+        current learning rate depending on remaining progress
+    """
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        if progress_remaining > 0.25:
+            return progress_remaining * initial_value
+        else:
+            return final_value
+
+    return func
