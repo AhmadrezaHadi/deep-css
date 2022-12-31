ab30c656585ae594dbb408bb4b55a849e8b66519
Ahmadreza Hadi <ahmadreza.hadi7@gmail.com>
2022-12-31 17:42:27 +0330

    modified main function

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from stable_baselines3 import PPO
 
 from utils import calculate_average_slowdown, SJF, make_env
 from arg_loader import arg_parser
-from envs.deepcss_v0.environment import Parameters
+from envs.deepcss_v0.environment import Parameters as V0_PA
+from envs.deepcss_v1.environment import Parameters as V1_PA
 
 
 def make_eval_envs(pa):
@@ -22,21 +23,14 @@ def make_eval_envs(pa):
 
 def main():
     args = arg_parser()
-    ENV_ID = 'deepcss-v0'
+    ENV_ID = args.environment
     TIMESTEPS = args.timesteps
     CPU = args.cpu
     CLIP_FN = get_schedule_fn(args.cliprange)
 
     vec_envs = make_vec_env(ENV_ID, n_envs=CPU)
 
-    vf_net = [128, 256, 128, 64]
-    pi_net = [128, 256, 128, 64]
-    policy_kwargs = {
-        "net_arch": [{
-            "vf": vf_net,
-            "pi": pi_net
-        }]
-    }
+    policy_kwargs = V1_PA().policy_kwargs
 
     if args.mode == 'train':
         if not args.name:
@@ -45,10 +39,10 @@ def main():
         else:
             MODEL_NAME = args.name
         checkpoint_callback = CheckpointCallback(save_freq=5_000,
-                                                 save_path=f'./models/{args.algorithm}_{MODEL_NAME}',
+                                                 save_path=f'./models/{MODEL_NAME}_{args.algorithm}',
                                                  name_prefix=f'{args.algorithm}')
-        eval_callback = EvalCallback(vec_envs, best_model_save_path='./logs/',
-                                     log_path='./logs/', eval_freq=2_500, deterministic=True, render=False)
+        eval_callback = EvalCallback(vec_envs, best_model_save_path='./logs/{MODEL_NAME}/',
+                                     log_path='./logs/{MODEL_NAME}/', eval_freq=2_500, deterministic=True, render=False)
         callbacks = CallbackList([checkpoint_callback, eval_callback])
 
         if args.algorithm == 'ppo':
@@ -63,7 +57,7 @@ def main():
             try:
                 print("training")
                 model.learn(TIMESTEPS, callback=callbacks,
-                            tb_log_name=f'{args.algorithm}_{MODEL_NAME}')
+                            tb_log_name=f'{MODEL_NAME}_{args.algorithm}')
             except:
                 model.save('tmp/last_model')
                 print(f"model trained using {args.algorithm} algorithm.")
